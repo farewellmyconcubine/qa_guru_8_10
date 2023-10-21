bd1f826f9725f70f4c508ca01010fdd7f557d9c8
Vadzim Belashtsentau <vadzimbelashtsentau@gmail.com>
2023-10-21 21:41:34 +0300

    add classes and methods II

--- qa_guru_8_10/pages/registration_page.py
+++ qa_guru_8_10/pages/registration_page.py
@@ -7,83 +7,44 @@ class RegistrationPage:
 
     def open(self):
         browser.open('/automation-practice-form')
-        return self
 
-    def fill_first_name(self, value):
-        browser.element('#firstName').type(value)
-        return self
-
-    def fill_last_name(self, value):
-        browser.element('#lastName').type(value)
-        return self
-
-    def fill_email(self, value):
-        browser.element('#userEmail').type(value)
-        return self
-
-    def choose_gender(self, value):
-        browser.all('[name=gender]').element_by(have.value(value)).element('..').click()
-        return self
-
-    def fill_mobile_number(self, value):
-        browser.element('#userNumber').type(value)
-        return self
-
-    def fill_date_of_birth(self, year, month, day):
+    def register(self, user):
+        browser.element('#firstName').type(user.first_name)
+        browser.element('#lastName').type(user.last_name)
+        browser.element('#userEmail').type(user.email)
+        browser.all('[name=gender]').element_by(have.value(user.gender)).element('..').click()
+        browser.element('#userNumber').type(user.mobile_number)
         browser.element('#dateOfBirthInput').click()
-        browser.element('.react-datepicker__month-select').type(month)
-        browser.element('.react-datepicker__year-select').type(year)
+        browser.element('.react-datepicker__month-select').type(user.month_of_birth)
+        browser.element('.react-datepicker__year-select').type(user.year_of_birth)
         browser.element(
-            f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)'
+            f'.react-datepicker__day--0{user.day_of_birth}:not(.react-datepicker__day--outside-month)'
         ).click()
-        return self
-
-    def choose_subject(self, value):
-        browser.element('#subjectsInput').type(value).press_enter()
-        return self
-
-    def choose_hobbie(self, value):
-        browser.all('.custom-checkbox').element_by(have.exact_text(value)).click()
-        return self
-
-    def select_picture(self, image_name):
-        browser.element('#uploadPicture').send_keys(resources.path(image_name))
-        return self
-
-    def fill_adress(self, value):
-        browser.element('#currentAddress').type(value)
-        return self
-
-    def fill_state(self, name):
+        browser.element('#subjectsInput').type(user.subject).press_enter()
+        browser.all('.custom-checkbox').element_by(have.exact_text(user.hobby)).click()
+        browser.element('#uploadPicture').send_keys(resources.path(user.image))
+        browser.element('#currentAddress').type(user.current_address)
         browser.element('#state').click()
         browser.all('[id^=react-select][id*=option]').element_by(
-            have.exact_text(name)
+            have.exact_text(user.state)
         ).click()
-        return self
-
-    def fill_city(self, name):
         browser.element('#city').click()
         browser.all('[id^=react-select][id*=option]').element_by(
-            have.exact_text(name)
+            have.exact_text(user.city)
         ).click()
-        return self
-
-    def submit_form(self):
         browser.element('#submit').click()
-        return self
 
-    def should_have_registered(self, full_name, email, gender, phone_number, date, subject, hobbie, image, address,
-                               state_and_city):
+    def should_have_registered(self, user):
         browser.element('.table').all('tr td:nth-child(2)').should(have.texts
             (
-            full_name,
-            email,
-            gender,
-            phone_number,
-            date,
-            subject,
-            hobbie,
-            image,
-            address,
-            state_and_city
+            f'{user.first_name} {user.last_name}',
+            user.email,
+            user.gender,
+            user.mobile_number,
+            f'{user.day_of_birth} {user.month_of_birth},{user.year_of_birth}',
+            user.subject,
+            user.hobby,
+            user.image,
+            user.current_address,
+            f'{user.state} {user.city}'
         ))
